50bd493fbdbfb3cb910ffd5c1f1363a4b242ade4
mudrunekfilip <filip.mudrunek@gmail.com>
2015-05-06 00:37:04 +0200

    test rename

--- robot_test1.py
+++ robot_test1.py
@@ -1,14 +1,15 @@
 import cv2
 import sys
-from pre_processing import *
-from speech import *
-from shapes import *
-from enums import *
 from config import *
 from nao_controller import *
 
 
 cam_img = get_image_from_camera(ROBOT_IP, ROBOT_PORT)
+print("Retrieved CV2 image:")
+print("\trows,cols,channels: ", cam_img.shape)
+print("\t# pixels= ", cam_img.size)
+print("\ttype: ", cam_img.dtype)
+
 cv2.imshow('robot_view', cam_img)
 cv2.waitKey(0)
 sys.exit(0)
